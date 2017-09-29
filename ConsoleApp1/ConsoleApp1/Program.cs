8d024b69eea064d4e678cd309289c01c5aae0b5d
Gregory Dukes <kcils360@gmail.com>
2017-09-28 19:38:29 -0700

    fixed program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,32 +1,13 @@
 ﻿using System;
 
+
 namespace DataStructures
 {
     class Program
     {
         static void Main(string[] args)
         {
-            public bool IsPalendrome()
-            {
-                if (head == null)
-                {
-                    throw new Exception();
-                }
-
-                Node f = Head;
-                Node b = Tail;
-                While(f.Data == b.Data)
-                {
-                    f = f.Next;
-                    b = b.Next;
-
-                    if (f.Next == b.Next || f.Next = b)
-                    {
-                        return true;
-                    }
-
-                }
-                return false;
-            }
+            Palendrome one = new Palendrome();
+        }
     }
 }
