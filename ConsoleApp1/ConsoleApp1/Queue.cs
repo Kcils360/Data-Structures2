dd1930bc71a6e9d4aba6229e65b9865790a5346b
Gregory Dukes <kcils360@gmail.com>
2017-09-28 20:15:54 -0700

    Queue of stacks. Turning in for credit

---
+++ ConsoleApp1/ConsoleApp1/Queue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class Queue : Stacks
+    {
+        Stacks one = new Stacks();
+        Stacks two = new Stacks();
+
+        public Queue()
+        {
+            Node Head = one;
+            Node Tail = Head;
+        }
+
+        public void Enqueue()
+        {
+            Tail.Next = two;
+            Tail = Tail.Next;
+        }
+    }
+}
